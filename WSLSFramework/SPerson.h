2de229584b1017ea12255aba01c524ced55aac27
admin <admin@admindeiMac.local>
2020-01-11 15:47:50 +0800

    初始化

---
+++ WSLSFramework/SPerson.h
@@ -0,0 +1,17 @@
+//
+//  SPerson.h
+//  WSLSFramework
+//
+//  Created by admin on 2020/1/11.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WSLSFramework/SPerson.m
@@ -0,0 +1,13 @@
+//
+//  SPerson.m
+//  WSLSFramework
+//
+//  Created by admin on 2020/1/11.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import "SPerson.h"
+
+@implementation SPerson
+
+@end
